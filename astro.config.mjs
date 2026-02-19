1bb4fab4b158e81a7fba70e8ed68acf9f42e96ce
Betty Bergqvist <bbe@bth.se>
2026-02-19 10:39:44 +0100

    kopierat tillbaka urspungsinnehållet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,91 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 import starlight from '@astrojs/starlight';
-import starlightImageZoom from 'starlight-image-zoom'
-import embeds from 'astro-embed/integration';
-import starlightAutoSidebar from 'starlight-auto-sidebar'
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-	},
 	integrations: [
-		embeds(),
 		starlight({
-			plugins: [
-				starlightImageZoom(),
-				starlightAutoSidebar(), // https://starlight-auto-sidebar.netlify.app/metadata/#label
-			],
-			title: 'Kursen CSharp',
-			favicon: 'favicon.png',
-			logo: {
-				src: '@assets/leaf_256x256.png',
-			},
-			customCss: [
-				'./src/styles/dbwebb.css',
-			],
-			editLink: {
-				baseUrl: 'https://github.com/bth-csharp/website/tree/main',
-			},
-			social: [
-				{
-					icon: 'github',
-          			label: 'GitHub',
-					href: 'https://github.com/bth-csharp/website'
-				}
-			],
-			head: [
-				{
-					tag: 'script',
-					attrs: {
-						src: '/website/js/OpenDetailsFromHash.js',
-						defer: true,
-					},
-				},
-				{
-					tag: 'script',
-					attrs: {
-						src: '/website/js/openIssue.js',
-						defer: true,
-					},
-				},
-				{
-					tag: 'base',
-					attrs: {
-						href: ''
-					}
-				}
-			],
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Introduktion',
-					collapsed: true,
-					autogenerate: { directory: 'intro' },
-				},
-				{
-					label: 'Kursmoment',
-					collapsed: true,
-					autogenerate: { directory: 'kmom' },
-				},
-				{
-					label: 'Läromaterial',
-					collapsed: true,
-					autogenerate: { directory: 'laromaterial' },
-				},
-				{
-					label: 'Studieguide',
-					collapsed: true,
-					autogenerate: { directory: 'studieguide' },
-					/* items: [
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
-					],*/
+					],
 				},
 				{
-					label: 'Övrigt',
-					collapsed: true,
-					autogenerate: { directory: 'ovrigt' },
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
